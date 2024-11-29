5a4b5cbf2b4ea52f03da03f7d01f4995c8be6ee7
Gustyx Power <gustiadityamuzaky08@gmail.com>
2024-11-30 00:39:45 +0700

    Fix Error in activity_login.xml , AndroidManifest.xml , LoginActivity.kt & Fixed Errno(5) Fixed poor integration between layout and kotlin classes, Fixed Missing code Errno (5)

--- app/src/main/java/com/android/gustyx/officedesk/LoginActivity.kt
+++ app/src/main/java/com/android/gustyx/officedesk/LoginActivity.kt
@@ -1,36 +1,13 @@
 package com.android.gustyx.officedesk
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import java.io.IOException
-import java.io.InputStream
 
 class LoginActivity : AppCompatActivity() {
-
-    private lateinit var imageViewLogo: ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        imageViewLogo = findViewById(R.id.imageViewLogo)
-        loadImageFromAssets("drawable/texthome.png")
     }
 
-    private fun loadImageFromAssets(fileName: String) {
-        try {
-            val inputStream: InputStream = assets.open(fileName)
-            val bitmap: Bitmap = BitmapFactory.decodeStream(inputStream)
-            imageViewLogo.setImageBitmap(bitmap)
-            inputStream.close()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
-}
-
-    }
 }
