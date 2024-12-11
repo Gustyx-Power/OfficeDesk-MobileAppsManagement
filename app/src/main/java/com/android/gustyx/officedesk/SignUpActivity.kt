5cc3ec7e0f9d2366a463b8e222040ace131eb9c1
Gustyx Power <gustiadityamuzaky08@gmail.com>
2024-12-12 01:31:15 +0700

    Adding Multiple Layers of Security with Biometric Security from Android System when Signing UP

--- app/src/main/java/com/android/gustyx/officedesk/SignUpActivity.kt
+++ app/src/main/java/com/android/gustyx/officedesk/SignUpActivity.kt
@@ -19,7 +19,39 @@ class SignUpActivity : AppCompatActivity() {
         val usernameEditText = findViewById<EditText>(R.id.username)
         val passwordEditText = findViewById<EditText>(R.id.password)
         val signUpButton = findViewById<ImageButton>(R.id.signup)
+        val loginButton = findViewById<ImageButton>(R.id.login)
 
+        // Konfigurasi BiometricPrompt
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(applicationContext, "Autentikasi error: $errString", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    val username = usernameEditText.text.toString()
+                    val password = passwordEditText.text.toString()
+                    if (username.isNotEmpty() && password.isNotEmpty()) {
+                        val newUser = User(username = username, password = password)
+                        viewModel.insert(newUser)
+                        Toast.makeText(applicationContext, "Daftar Berhasil", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(applicationContext, "Autentikasi gagal", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Verifikasi Biometrik")
+            .setSubtitle("Gunakan sidik jari atau face unlock untuk verifikasi")
+            .setNegativeButtonText("Batalkan")
+            .build()
 
         signUpButton.setOnClickListener {
             val username = usernameEditText.text.toString()
